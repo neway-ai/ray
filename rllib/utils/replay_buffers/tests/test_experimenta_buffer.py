97addc1a0d6edfc76fea05ba616099798e6f6c4e
Artur Niederfahrenhorst <artur@anyscale.com>
2022-12-03 10:21:06 -0800

    fix test
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/utils/replay_buffers/tests/test_experimenta_buffer.py
+++ rllib/utils/replay_buffers/tests/test_experimenta_buffer.py
@@ -1,21 +1,38 @@
-from ray.rllib.algorithms.dqn import DQN, DQNConfig
 import gym
+import unittest
 
-dummy_env = gym.make("CartPole-v0")
-action_space = dummy_env.action_space
-observation_space = dummy_env.observation_space
+from ray import tune, air
+from ray.rllib.algorithms.dqn import DQN, DQNConfig
 
-config = DQNConfig().environment(
-    env="CartPole-v0", observation_space=observation_space, action_space=action_space
-)
 
-buffer_config = {
-    "type": "MultiAgentExperimentalReplayBuffer",
-    "observation_space": observation_space,
-    "action_space": action_space,
-    "model_config": config.model,
-}
+class TestExperimentalReplayBuffer(unittest.TestCase):
+    def test_execution(self):
+        dummy_env = gym.make("CartPole-v0")
+        action_space = dummy_env.action_space
+        observation_space = dummy_env.observation_space
 
-config.training(replay_buffer_config=buffer_config)
+        config = DQNConfig().environment(env="CartPole-v0",
+                                         observation_space=observation_space,
+                                         action_space=action_space)
 
-trainer = DQN("Pendulum-v1", config=config.to_dict())
+        buffer_config = {
+                "type": "MultiAgentExperimentalReplayBuffer",
+                "observation_space": observation_space,
+                "action_space": action_space,
+                "model_config": config.model,
+        }
+        stop_config = {
+            "training_iteration": 10,
+        }
+
+        config.training(replay_buffer_config=buffer_config)
+
+        tuner = tune.Tuner(
+            DQN,
+            param_space=config,
+            run_config=air.RunConfig(
+                stop=stop_config,
+            ),
+        )
+
+        tuner.fit()
